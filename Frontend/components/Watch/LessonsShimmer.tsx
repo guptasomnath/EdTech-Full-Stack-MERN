99bf21e134b0bae6667cf330a6b8bc80dd761c42
Somnath Gupta <sapphelper@gmail.com>
2024-01-31 12:55:40 +0530

    Updated the README.md file and resolved the UI issue for mobile devices on the watch page.

--- Frontend/components/Watch/LessonsShimmer.tsx
+++ Frontend/components/Watch/LessonsShimmer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LessonsShimmer() {
   return (
-    <div className='w-full px-10'>
+    <div className='w-full px-10 sm:px-2 sm:mt-5'>
         <div className='w-full h-[1.8rem] bg-slate-100 shimmerAnimation'></div>
         <div className='w-full mt-2 h-[1.8rem] bg-slate-100 shimmerAnimation'></div>
         <div className='w-full mt-2 h-[1.8rem] bg-slate-100 shimmerAnimation'></div>
